6b51cdf6635274aa0a5376c2c2baaf0f6a902f02
tim <tim.koeppel1@gmail.com>
2025-07-20 03:01:54 +0200

    make jwt signature valid

--- sundowner-service/logic/src/main/kotlin/de/tkoeppel/sundowner/security/jwt/JwtService.kt
+++ sundowner-service/logic/src/main/kotlin/de/tkoeppel/sundowner/security/jwt/JwtService.kt
@@ -4,16 +4,19 @@ import io.jsonwebtoken.Claims
 import io.jsonwebtoken.Jwts
 import org.springframework.stereotype.Service
 import java.util.*
+import javax.crypto.SecretKey
 import javax.crypto.spec.SecretKeySpec
 
 
 @Service
 class JwtService(private val jwtConfig: JwtConfig) {
-	private val signingKey: SecretKeySpec
-		get() {
-			val keyBytes: ByteArray = Base64.getDecoder().decode(this.jwtConfig.secretKey)
-			return SecretKeySpec(keyBytes, 0, keyBytes.size, "HmacSHA256")
+	private val signingKey: SecretKey by lazy {
+		val keyBytes = jwtConfig.secretKey.toByteArray()
+		require(keyBytes.size >= 32) {
+			"Secret key for HmacSHA256 must be at least 256 bits (32 bytes) long."
 		}
+		SecretKeySpec(keyBytes, "HmacSHA256")
+	}
 
 	fun generateToken(subject: String, expiresAt: Date, additionalClaims: Map<String, Any> = emptyMap()): String {
 		return Jwts.builder() //
